7bc4b8dbaa58b51ffaf747df43a4513a603ccd81
Wei Kang <weikangchia@gmail.com>
2019-03-03 21:28:55 +0800

    Update helloWorld.js

--- modules/sample/endpoints/helloWorld.js
+++ modules/sample/endpoints/helloWorld.js
@@ -4,7 +4,7 @@ const handlerWrapper = require('../../../shared/utils/handlerWrapper');
 const responseUtil = require('../../../shared/utils/responseUtil');
 
 module.exports.handler = handlerWrapper(async (event) => {
-  log.info(${process.env.ENVIRONMENT});
+  log.info(process.env.ENVIRONMENT);
   const response = responseUtil.createResponse({
     data: 'hello world',
   });
